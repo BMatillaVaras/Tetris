34a35c9c277add0879fb7896c6026ebbf38896fc
BMatillaVaras <bmatillavaras@gmail.com>
2020-12-22 13:57:36 +0100

    add tetrominoes

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,71 @@
 "use strict";
+
+const grid = document.querySelector(".grid");
+let squares = Array.from(document.querySelectorAll(".grid div"));
+const scoreDisplay = document.querySelector("#score");
+const startBtn = document.querySelector("#start- button");
+
+const width = 10;
+
+// The tetrominoes
+
+const lTetromino = [
+  [1, width + 1, width * 2 + 1, 2],
+  [width, width + 1, width + 2, width * 2 + 2],
+  [1, width + 1, width * 2 + 1, width * 2],
+  [width, width * 2, width * 2 + 1, width * 2 + 2],
+];
+
+const uTetromino = [
+  [0, 1, width + 1, width * 2 + 1],
+  [2, width, width + 1, width + 2],
+  [1, width + 1, width * 2 + 1, width * 2 + 2],
+  [width, width + 1, width + 2, width * 2],
+];
+
+const zTetromino = [
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+];
+
+const sTetromino = [
+  [width, width + 1, width * 2 + 1, width * 2 + 2],
+  [2, width + 1, width + 2, width * 2 + 1],
+  [width, width + 1, width * 2 + 1, width * 2 + 2],
+  [2, width + 1, width + 2, width * 2 + 1],
+];
+
+const tTetromino = [
+  [1, width, width + 1, width + 2],
+  [1, width + 1, width + 2, width * 2 + 1],
+  [width, width + 1, width + 2, width * 2 + 1],
+  [1, width, width + 1, width * 2 + 1],
+];
+
+const oTetromino = [
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+];
+
+const iTetromino = [
+  [width, width + 1, width + 2, width + 3],
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+  [1, width + 2, width * 2 + 1, width * 3 + 1],
+];
+
+const theTetrominoes = [
+  lTetromino,
+  uTetromino,
+  zTetromino,
+  sTetromino,
+  tTetromino,
+  oTetromino,
+  iTetromino,
+];
+
+console.log(squares);
