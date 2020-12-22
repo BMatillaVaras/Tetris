6eee9c14bc10fdeb257d1e68a6d7a2de4250d760
BMatillaVaras <bmatillavaras@gmail.com>
2020-12-22 14:58:42 +0100

    draw the first rotation in the first tetromino

--- src/js/main.js
+++ src/js/main.js
@@ -68,4 +68,15 @@ const theTetrominoes = [
   iTetromino,
 ];
 
-console.log(squares);
+let currentPosition = 4;
+let current = theTetrominoes[0][0];
+
+//draw the first rotation in the first tetromino
+
+function draw() {
+  current.forEach((index) => {
+    squares[currentPosition + index].classList.add("tetromino");
+  });
+}
+
+draw();
